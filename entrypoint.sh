a274622227565c66d61bad3b5bff83db4a1ec60d
msftsettiy <63882775+msftsettiy@users.noreply.github.com>
2023-10-25 10:52:17 -0700

    Bumped up the CCF version to 4.0.7 and added logging in the entrypoint script (#5)
    Co-authored-by: Yagnesh Setti <settiy@microsoft.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ echo "$CERTD" > /opt/ccf_sgx/bin/cert
 echo "$KEYD" > /opt/ccf_sgx/bin/key
 
 cd /opt/ccf_sgx/bin
+echo "Submitting the JS app."
 content=$(ccf_cose_sign1 --ccf-gov-msg-type proposal --ccf-gov-msg-created_at `date -Is` --signing-key key --signing-cert cert --content set_js_app.json | curl ${CCF_URL}/gov/proposals -k -H "content-type: application/cose" --data-binary @-)
+echo "Response from CCF: $content"
 proposal=$(echo "${content}" | jq '.proposal_id')
+echo "The proposal_id: $proposal"
 echo "proposal=$proposal" >> $GITHUB_OUTPUT
